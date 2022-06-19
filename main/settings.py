77826804b2bbe49a8d8698bd20ace6b04d96e552
Ahmed Salah <toushi2009@gmail.com>
2022-06-19 13:34:39 +0200

    added database settings

--- main/settings.py
+++ main/settings.py
@@ -74,9 +74,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'fakebok',
+
+        'USER': 'ahmed',
+
+        'PASSWORD': '3766',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
